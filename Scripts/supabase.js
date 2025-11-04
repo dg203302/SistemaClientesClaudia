d87e7c974d77f4bc2aa978daa136fe0e2f128eda
Diego <168243435+dg203302@users.noreply.github.com>
2025-11-03 23:25:27 -0300

    feat: add session management button and update client administration link in the admin panel

---
+++ Scripts/supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://qxbkfmvugutmggqwxhrb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YmtmbXZ1Z3V0bWdncXd4aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTEzMDEsImV4cCI6MjA3MzgyNzMwMX0.Qsx0XpQaSgt2dKUaLs8GvMmH8Qt6Dp_TQM25a_WOa8E'
+const { createClient } = supabase
+const client = createClient(supabaseUrl, supabaseKey)
